eef6929315d96e9f19315060f4badef04676ba1f
andinhodelgado <anderson.sdelgado@gmail.com>
2018-12-07 12:17:59 -0200

    Ajuste na Verificação de OS.

--- dao/VerifOSDAO.class.php
+++ dao/VerifOSDAO.class.php
@@ -29,9 +29,9 @@ class VerifOSDAO extends Conn {
                 . " FROM "
                     . " VMB_OS_AUTO "
                 . " WHERE "
-                    . " NRO = " . $valor
-                    . " AND DT_PREV_INIC <= SYSDATE " 
-                    . " AND DT_PREV_TERM >= SYSDATE - 1 " ;
+                    . " NRO = " . $valor;
+//                    . " AND DT_PREV_INIC <= SYSDATE " 
+//                    . " AND DT_PREV_TERM >= SYSDATE - 1 " ;;
         
         $this->Conn = parent::getConn();
         $this->Read = $this->Conn->prepare($select);
@@ -43,16 +43,19 @@ class VerifOSDAO extends Conn {
         $res1 = json_encode($dados);
         
         $select = " SELECT "
-                . " OS_ID AS \"idOS\" "
-                . " , NRO AS \"nroOS\" "
-                . " , NRO_EQUIP AS \"equipOS\" "
+                . " I.ITOSMECAN_ID AS \"idItemOS\" "
+                . " , I.OS_ID AS \"idOS\" "
+                . " , I.ITEM_OS AS \"seqItemOS\" "
+                . " , I.SERVICO_ID AS \"idServItemOS\" "
+                . " , I.COMPONENTE_ID AS \"idCompItemOS\" "
                 . " FROM "
-                . " USINAS.VMB_OS_AUTO "
+                . " USINAS.VMB_OS_AUTO OS "
+                . " , USINAS.VMB_ITEM_OS_AUTO I "
                 . " WHERE "
                 . " OS.NRO = " . $valor
-                . " I.OS_ID = OS.OS_ID "
-                . " AND OS.DT_PREV_INIC <= SYSDATE "
-                . " AND OS.DT_PREV_TERM >= SYSDATE - 1 ";
+                . " AND I.OS_ID = OS.OS_ID ";
+//                . " AND OS.DT_PREV_INIC <= SYSDATE "
+//                . " AND OS.DT_PREV_TERM >= SYSDATE - 1 ";
         
         $this->Conn = parent::getConn();
         $this->Read = $this->Conn->prepare($select);
